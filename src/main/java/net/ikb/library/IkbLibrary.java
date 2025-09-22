c35bb1eb3756f1f4a81aaee3ccb44a20d2d1f81c
ikkyblob <ikkyblob@gmail.com>
2025-09-22 17:11:15 -0400

    deprecated fix

--- src/main/java/net/ikb/library/IkbLibrary.java
+++ src/main/java/net/ikb/library/IkbLibrary.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
